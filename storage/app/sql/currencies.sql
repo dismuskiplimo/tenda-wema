d82db426c60b31187df59a501368798304308cb3
Dismus Ng'eno <dismuskiplimo@gmail.com>
2022-06-13 02:07:03 +0300

    fixed missing sql migration files

--- storage/app/sql/currencies.sql
+++ storage/app/sql/currencies.sql
@@ -1,4 +1,4 @@
 INSERT INTO currencies (country, currency, code, symbol) VALUES
-    ("Kenya", "Kenyan Shilling","KES","KES"),
-    ("United Kingdom", "British Pound","GBP","£"),
-    ("United States of America", "US Dollar","USD","$");
+    ('Kenya', 'Kenyan Shilling','KES','KES'),
+    ('United Kingdom', 'British Pound','GBP','£'),
+    ('United States of America', 'US Dollar','USD','$');
